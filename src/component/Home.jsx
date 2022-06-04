beb2e448179b9822ebd7a86609e44f5c4fffdb71
Pulkit Singh <pulkitsingh01@gmail.com>
2022-06-04 16:20:13 +0530

    table rendered

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Exmployees from "./Employees";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
-  
-    render() {
-        return ( <h2 className="text-center">Home</h2>);
-    }
-
+  render() {
+    return (
+      <div>
+        <h2 className="text-center">Home</h2>
+        <Exmployees />
+      </div>
+    );
+  }
 }
 
 export default Home;
