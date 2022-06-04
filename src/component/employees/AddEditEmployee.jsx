388f7739a07d26459d4be9cee3599e809912ee0e
Pulkit Singh <pulkitsingh01@gmail.com>
2022-06-04 17:24:08 +0530

    add employee

--- src/component/employees/AddEditEmployee.jsx
+++ src/component/employees/AddEditEmployee.jsx
@@ -1,45 +1,101 @@
 import React from "react";
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button } from "react-bootstrap";
+import { addEmployee } from "./data";
+import { useNavigate } from "react-router-dom";
 
-const AddEditEmployee = () => {
+const AddEditEmployee = ({
+  initialData = {
+    firstName: undefined,
+    lastName: undefined,
+    dob: undefined,
+    designation: undefined,
+    profilePic: undefined,
+    experience: undefined,
+  },
+}) => {
+  const [values, setValues] = React.useState(initialData);
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const form = e.currentTarget.value;
-    console.log({ e, form });
+    addEmployee(values);
+    navigate("/employees", { replace: true });
   };
 
   return (
     <div>
-      <h2>Add Employee</h2>
+      <h2>{initialData ? "Edit Employee" : "Add Employee"}</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="firstName">
           <Form.Label>First Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter First Name" />
+          <Form.Control
+            type="text"
+            placeholder="Enter First Name"
+            value={values.firstName}
+            onChange={(e) => {
+              setValues({ ...values, firstName: e.currentTarget.value });
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="lastName">
           <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Last Name" />
+          <Form.Control
+            type="text"
+            placeholder="Enter Last Name"
+            value={values.lastName}
+            onChange={(e) => {
+              setValues({ ...values, lastName: e.currentTarget.value });
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="dob">
           <Form.Label>DOB</Form.Label>
-          <Form.Control type="text" placeholder="Enter Date of Birth" />
+          <Form.Control
+            type="text"
+            placeholder="Enter Date of Birth"
+            value={values.dob}
+            onChange={(e) => {
+              setValues({ ...values, dob: e.currentTarget.value });
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="designation">
           <Form.Label>Designation</Form.Label>
-          <Form.Control type="text" placeholder="Enter Designation" />
+          <Form.Control
+            type="text"
+            placeholder="Enter Designation"
+            value={values.designation}
+            onChange={(e) => {
+              setValues({ ...values, designation: e.currentTarget.value });
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="experience">
           <Form.Label>Experience</Form.Label>
-          <Form.Control type="text" placeholder="Enter Designation" />
+          <Form.Control
+            type="text"
+            placeholder="Enter Experience"
+            value={values.experience}
+            onChange={(e) => {
+              setValues({ ...values, experience: e.currentTarget.value });
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="profilePic">
           <Form.Label>Profile Pic URL</Form.Label>
-          <Form.Control type="text" placeholder="Enter Profile Pic URL" />
+          <Form.Control
+            type="text"
+            placeholder="Enter Profile Pic URL"
+            value={values.profilePic}
+            onChange={(e) => {
+              setValues({ ...values, profilePic: e.currentTarget.value });
+            }}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
